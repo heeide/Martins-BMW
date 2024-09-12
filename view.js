ecf041710eed7e8fd56b09420bd583876f599ca5
SudhaPuchala <sudhavardhann@gmail.com>
2024-09-12 13:35:56 +0200

    Add files via upload

--- view.js
+++ view.js
@@ -1,20 +1,7 @@
-function updateView(){
-    app.innerHTML = /*HTML*/ `
-    ${displayInventory()}
-    
-    `;
-
-}
-
-function displayInventory(){ //Viser inventory i en liste som bygges etterhvert som ting blir lagt til i inventory.
-    let html = '<ul>';
-    
-    for(i=0; i<inventory.length; i++){
-        html += '<li>' + inventory[i] + '</li>'
-        
-    }
-
-    html += '</ul>'
-    return html;
-
+// View Update Function
+function updateView(responseText, className) {
+    document.getElementById('coolness').textContent = carCoolness;
+    const responseElement = document.getElementById('response');
+    responseElement.textContent = responseText;
+    responseElement.className = className;
 }
