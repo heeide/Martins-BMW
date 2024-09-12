d4f3c554448bfff3e9c624ee39e2dcdd0cfe1148
TheSommerViking <andreassommervik@hotmail.com>
2024-09-12 10:55:21 +0200

    Revert "Add files via upload"
    This reverts commit 99be62e8c2c7467b5765cba563317a67498fad7c.

---
+++ model.js
@@ -0,0 +1,32 @@
+
+
+// objekter
+
+let Martin = {
+    greeting: "thumbs",
+}
+
+let Terje = {
+    greeting: "nod",
+}
+
+let Eskil = {
+    greeting: "wave",
+}
+
+let oldlady ={
+    action: "hit car"
+}
+let bump ={
+    action: "break car"
+}
+let wunderbaum ={
+    action: "good smell"
+}
+let spraypaint ={
+    action: "cool stripes"
+}
+
+
+let buddies = [Martin, Terje, Eskil]
+let events = [oldlady, bump, wunderbaum, spraypaint]
